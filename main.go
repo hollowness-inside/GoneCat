eda58473848fe440744508908b4d283459d97962
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-06-17 00:50:49 +1000

    Simple Connection implemented

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net"
 	"os"
 )
@@ -9,12 +11,16 @@ type Arguments struct {
 	Listening bool
 	Ipv4      bool
 	Ipv6      bool
-	Udp       bool
+	Tcp       bool
 	Address   net.TCPAddr
 }
 
 func main() {
 	args := Arguments{}
+	args.Listening = false
+	args.Ipv4 = true
+	args.Ipv6 = false
+	args.Tcp = true
 	arg := 1
 
 	for arg < len(os.Args) {
@@ -25,7 +31,7 @@ func main() {
 		case "-6":
 			args.Ipv6 = true
 		case "-u":
-			args.Udp = true
+			args.Tcp = false
 		case "-l":
 			args.Listening = true
 		default:
@@ -39,8 +45,58 @@ func main() {
 
 		arg++
 	}
+
+	err := execute(args)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
 }
 
 func help() {
 	println("Use: nc [-46ul] address:port")
+}
+
+func execute(args Arguments) error {
+	if args.Listening {
+		return DoListen(args)
+	} else {
+		return DoConnect(args)
+	}
+}
+
+func DoListen(args Arguments) error {
+	return nil
+}
+
+func DoConnect(args Arguments) error {
+	var network string
+	if args.Tcp {
+		network = "tcp"
+	} else {
+		network = "udp"
+	}
+
+	conn, err := net.Dial(network, args.Address.String())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	go func() {
+		io.Copy(os.Stdout, os.Stdin)
+		panic("Cannot copy from Stdin into Stdout")
+	}()
+
+	go func() {
+		io.Copy(conn, os.Stdin)
+		panic("Cannot copy from Stdin into Conn")
+	}()
+
+	go func() {
+		io.Copy(os.Stdout, conn)
+		panic("Cannot copy from Conn into Stdout")
+	}()
+
+	for {
+	}
 }
