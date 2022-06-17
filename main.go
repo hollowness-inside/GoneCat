b3e09f3c62b1057ba90a4a9e08430aef140f848f
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-06-17 21:15:14 +1000

    Functions migrated into GoneCat struct

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-type Arguments struct {
+type GoneCat struct {
 	Listening bool
 	Ipv4      bool
 	Ipv6      bool
@@ -17,75 +17,36 @@ type Arguments struct {
 	Addr      net.TCPAddr
 }
 
-func (a Arguments) Default() Arguments {
-	a.Listening = false
-	a.Ipv4 = true
-	a.Ipv6 = false
-	a.Tcp = true
-	a.Addr = net.TCPAddr{}
-
-	return a
+func (gc *GoneCat) UseDefaults() {
+	gc.Listening = false
+	gc.Ipv4 = true
+	gc.Ipv6 = false
+	gc.Tcp = true
+	gc.Addr = net.TCPAddr{}
 }
 
-func (a *Arguments) Network() string {
-	if a.Tcp {
+func (gc *GoneCat) Network() string {
+	if gc.Tcp {
 		return "tcp"
 	} else {
 		return "udp"
 	}
 }
 
-func (a *Arguments) Address() string {
-	return a.Addr.String()
+func (gc *GoneCat) Address() string {
+	return gc.Addr.String()
 }
 
-func main() {
-	args := Arguments{}.Default()
-	arg := 1
-
-	for arg < len(os.Args) {
-		cur := os.Args[arg]
-		switch cur {
-		case "-4":
-			args.Ipv4 = true
-		case "-6":
-			args.Ipv6 = true
-		case "-u":
-			args.Tcp = false
-		case "-l":
-			args.Listening = true
-		default:
-			addr, err := net.ResolveTCPAddr("", cur)
-			if err != nil {
-				help()
-				return
-			}
-			args.Addr = *addr
-		}
-
-		arg++
-	}
-
-	err := Execute(args)
-	if err != nil {
-		fmt.Printf("err: %v\n", err)
-	}
-}
-
-func help() {
-	println("Use: nc [-46ul] address:port")
-}
-
-func Execute(args Arguments) error {
-	if args.Listening {
-		return doListen(args)
+func (gc *GoneCat) Execute() error {
+	if gc.Listening {
+		return gc.doListen()
 	} else {
-		return doConnect(args)
+		return gc.doConnect()
 	}
 }
 
-func doListen(args Arguments) error {
-	listener, err := net.Listen(args.Network(), args.Address())
+func (gc *GoneCat) doListen() error {
+	listener, err := net.Listen(gc.Network(), gc.Address())
 	if err != nil {
 		return err
 	}
@@ -97,8 +58,8 @@ func doListen(args Arguments) error {
 	}
 }
 
-func doConnect(args Arguments) error {
-	conn, err := net.Dial(args.Network(), args.Address())
+func (gc *GoneCat) doConnect() error {
+	conn, err := net.Dial(gc.Network(), gc.Address())
 	if err != nil {
 		return err
 	}
@@ -120,8 +81,51 @@ func handleConnection(conn net.Conn) {
 		}
 	}()
 
-	go io.Copy(os.Stdout, conn)
+	io.Copy(os.Stdout, conn)
+}
 
-	for {
+func help() {
+	println("Use: gnc [options] address:port")
+	println("\t-u - Use UDP connection")
+	println("\t-t - Use TCP connection (Default)")
+	println("\t-4 - Use only IPv4")
+	println("\t-6 - Use only IPv6")
+}
+
+func main() {
+	args := GoneCat{}
+	args.UseDefaults()
+
+	arg := 1
+
+	for arg < len(os.Args) {
+		cur := os.Args[arg]
+		switch cur {
+		case "-4":
+			args.Ipv4 = true
+		case "-6":
+			args.Ipv6 = true
+		case "-u":
+			args.Tcp = false
+		case "-l":
+			args.Listening = true
+		case "-h", "--help":
+			help()
+			return
+		default:
+			addr, err := net.ResolveTCPAddr("", cur)
+			if err != nil {
+				help()
+				return
+			}
+			args.Addr = *addr
+		}
+
+		arg++
+	}
+
+	err := args.Execute()
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
 	}
 }
